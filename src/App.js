3f3ffc1fb492d6f28d2f96c88e4bcbdcb214b58a
agusgrance <agustingran@gmail.com>
2022-02-04 01:35:17 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import "./App.css";
 import Row from "./components/Row";
 import requests from "./requests";
 import Banner from "./components/Banner";
+import Nav from "./components/Nav";
 function App() {
   return (
     <div className="app">
-      {/* Nav */}
+      <Nav />
       <Banner />
       <Row
         title="Originales de Netflix"
